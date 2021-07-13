1d31544dedaa1e9d23afa990c18cce5661d27f77
pandepic <39251913+pandepic@users.noreply.github.com>
2021-07-14 00:42:15 +1000

    a bit less cursed

--- VNGUI/VNGUI/VeldridUtil.cs
+++ VNGUI/VNGUI/VeldridUtil.cs
@@ -11,17 +11,8 @@ namespace VNGUI
     {
         public static IntPtr GetD3D11ContextPtr(GraphicsDevice gd)
         {
-            var veldridAssembly = Assembly.Load(new AssemblyName("Veldrid"));
-            var d3d11GDType = veldridAssembly.GetType("Veldrid.D3D11.D3D11GraphicsDevice");
-
-            var d3d11GDContextField = d3d11GDType.GetFields(
-                         BindingFlags.NonPublic |
-                         BindingFlags.Instance).Where(f => f.Name == "_immediateContext").First();
-
-            var d3d11Context = d3d11GDContextField.GetValue(gd);
-            var d3d11ContextPtr = ((dynamic)d3d11Context).NativePointer;
-
-            return d3d11ContextPtr;
+            var dxDevice = new SharpDX.Direct3D11.Device(gd.GetD3D11Info().Device);
+            return dxDevice.ImmediateContext.NativePointer;
         }
     }
 }
